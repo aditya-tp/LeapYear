21bc8850f07aa2541ef6709ce4c5e42978e74bc6
unknown <adityatalalur.p@gmail.com>
2020-05-20 18:06:25 +0530

    second

--- leap/leapyear.java
+++ leap/leapyear.java
@@ -3,13 +3,19 @@ package leap;
 public class leapyear {
 public boolean isLeap(int year)
 {
-	if(year%4==0)
-	{
-		return true;
-	}
-	else
-	{
-		return false;
-	}
+	boolean result=ismultipleof4(year)&&isnotmultipleof100(year);
+	return result;
+}
+public boolean ismultipleof4(int year)
+{
+	return ismultipleof(year,4);
+}
+public boolean ismultipleof(int n,int base)
+{
+	return ((n%base)==0);
+}
+public boolean isnotmultipleof100(int year)
+{
+	return !ismultipleof(year,100);
 }
 }
--- leap/leapyeartest.java
+++ leap/leapyeartest.java
@@ -1,12 +1,21 @@
 package leap;
 import static org.junit.Assert.*;
-
+import org.junit.Before;
 import org.junit.Test;
 public class leapyeartest {
 	private leapyear lp;
+	@Before
+	public void setUp() throws Exception{
+		lp=new leapyear();
+	}
 @Test
 public void isnotleap_1900()
 {
 	assertFalse(lp.isLeap(1900));
+}
+@Test
+public void isleap_1996()
+{
+	assertTrue(lp.isLeap(1996));
 }
 }
